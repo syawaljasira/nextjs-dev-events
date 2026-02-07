66669a9b1e4bb10497c45e306ea4a6d8190655b2
syawaljasira <syawaljasira12@gmail.com>
2026-02-07 18:04:02 +0700

    fix let-const

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -29,8 +29,8 @@ export async function POST(req: NextRequest) {
       );
     }
 
-    let tags = JSON.parse((formData.get("tags") as string) || "[]");
-    let agenda = JSON.parse((formData.get("agenda") as string) || "[]");
+    const tags = JSON.parse((formData.get("tags") as string) || "[]");
+    const agenda = JSON.parse((formData.get("agenda") as string) || "[]");
 
     const arrayBuffer = await file.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
